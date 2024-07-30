d0836fb78600faaa8f88585a7bc27514395a90c4
Haruka31Dev <u202412824@upc.edu.pe>
2024-07-30 07:48:18 -0500

    generate:bundle project

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,8 @@
 export const environment = {
 
     production: false,
-    API_BASE: 'http://143.198.161.217/api/test/',
+    // API_BASE: 'http://143.198.161.217/api/test/',
+    API_BASE: 'http://127.0.0.1:8000/api/test/',   
     parametro: 'parametro',
 
 };
